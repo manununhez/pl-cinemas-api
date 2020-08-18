26d9f51e5fb32452c8a640a59cad263a69569043
Manuel Nunez <03.manu@gmail.com>
2020-08-18 15:54:44 +0200

    Database structure tables added

---
+++ api-laravel/database/migrations/2020_08_13_191603_create_movies_in_cinema_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesInCinemaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies_in_cinema', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('movie_id')->unsigned();
+            $table->bigInteger('cinema_id')->unsigned();
+            $table->timestamps();
+
+            $table->unique(['movie_id', 'cinema_id'], 'un_movies_cinema');
+
+            $table->foreign('movie_id')->references('id')->on('movies')->onDelete('cascade');
+            $table->foreign('cinema_id')->references('id')->on('cinemas')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies_in_cinema');
+    }
+}
